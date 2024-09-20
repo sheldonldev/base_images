9ea3b05f134db00e08688d01849b0c281e365af1
sheldonldev <xiadanli0320@gmail.com>
2024-09-20 11:21:12 +0000

    optimized

--- images/ie/ubuntu2204_py312/build_and_push.sh
+++ images/ie/ubuntu2204_py312/build_and_push.sh
@@ -5,3 +5,5 @@ source config.sh
 echo "build docker image with tag: $image_name"
 
 docker build -t $image_name -f Dockerfile .
+docker tag $image_name $image_name
+docker push $image_name
--- images/ie/ubuntu2204_py312/push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source config.sh
-
-docker tag $image_name $image_name
-docker push $image_name
