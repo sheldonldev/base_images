34d9228cfb2b77b54c19d0c6450f95b3bf93a0b6
sheldonldev <xiadanli0320@gmail.com>
2024-09-23 07:16:37 +0000

    simplify

--- images/ie/ubuntu2204_py312/build_and_push.sh
+++ images/ie/ubuntu2204_py312/build_and_push.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+tag=$1
+if [ -z $tag ]; then
+    echo 'Error: need input image tag'
+    exit 1
+fi
+
+namespace=sheldonlee
+name=ubuntu2204_py312
+image_name=$namespace/$name:$tag
 source config.sh
 
 echo "build docker image with tag: $image_name"
---
+++ images/ie/ubuntu2204_py312_chrome/README.md
@@ -0,0 +1,3 @@
+# README
+
+并没有打包驱动进去，只安装了 google. 需要把当前文件夹下的 `chromedrive` 挂载进去
