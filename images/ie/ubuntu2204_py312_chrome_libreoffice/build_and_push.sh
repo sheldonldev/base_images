37bc2b628893ebe7c73d942470192ded6f5d1930
sheldonldev <xiadanli0320@gmail.com>
2024-10-11 01:24:44 +0000

    optimized

---
+++ images/ie/ubuntu2204_py312_chrome_libreoffice/build_and_push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+tag=$1
+if [ -z $tag ]; then
+    echo 'Error: need input image tag'
+    exit 1
+fi
+
+namespace=sheldonlee
+name=ubuntu2204_py312_chrome_libreoffice
+image_name=$namespace/$name:$tag
+source config.sh
+
+echo "build docker image with tag: $image_name"
+
+docker build -t $image_name -f Dockerfile .
+docker tag $image_name $image_name
+docker push $image_name
